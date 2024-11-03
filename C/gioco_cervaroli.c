cea64aa61c31bb5ea9d41e6da3fc92e6c5967e92
SamueleCervaroli <99293854+PeraSlayer@users.noreply.github.com>
2024-11-03 12:49:21 +0100

    Create gioco_cervaroli.c

---
+++ C/gioco_cervaroli.c
@@ -0,0 +1,29 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int numero_utente; //input dell utente
+    srand(time(NULL)); //set randomizzazione
+    int r = rand()%100+1; //generazione numero random
+    
+    puts("BENVENUTO SU INDOVINA IL NUMERO\nHAI 5 TENTATIVI PER INDOVINARE UN NUMERO DA 1 A 100");
+    int i; //variabile ciclo
+    for(i = 0; i<=5; ++i){
+        printf("tentativo %d\n",i);
+        scanf("%d",&numero_utente);
+        //controllo input utente
+        if(numero_utente<r){
+            puts("numero troppo piccolo!");
+        }else if(numero_utente>r){
+            puts("numero troppo grande!");
+        }else{
+            puts("BRAVO HAI INDOVINATO, HAI VINTO YEEE");
+            return 0;
+        }
+    }
+    
+    printf("il numero era %d, prossima volta sarai + fortunato!",r);
+    return 0;
+}
